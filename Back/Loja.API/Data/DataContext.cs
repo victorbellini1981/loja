7d7fe62c1a3e6d1bb134ef0361f0091be82a162e
João Victor <joaovictorbellini33@gmail.com>
2021-08-30 22:21:54 -0300

    banco de dados criação

---
+++ Back/Loja.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Loja.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Loja.API.Data
+{
+    public class DataContext: DbContext
+    {
+       public DataContext(DbContextOptions<DataContext> options) :
+       base(options) {}
+
+       public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ Back/Loja.API/Data/Migrations/20210831011259_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Loja.API.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Estoque = table.Column<int>(type: "INTEGER", nullable: false),
+                    Valor = table.Column<double>(type: "REAL", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataAtualizacao = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
